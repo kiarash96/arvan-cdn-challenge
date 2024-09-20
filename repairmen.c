ae4db2b6165f88098611b0020247f84658d329f2
Kiarash Korki <kiarash.96@gmail.com>
2024-09-20 14:35:03 +0330

    Add shared memory and basic network grid

--- repairmen.c
+++ repairmen.c
@@ -1,22 +1,73 @@
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
 
 #include "repairmen.h"
 
-int agent(int id) {
+void initialize_shared_mem(shared_mem_t *mem) {
+    // Set fixed status for grid cells at random
+    for (int i = 0; i < GRID_SIZE; ++i)
+        for (int j = 0; j < GRID_SIZE; ++j) {
+            mem->grid[i][j].fixed = (bool) (rand() % 2);
+        }
+}
+
+void cleanup_shared_mem(shared_mem_t *mem) {
+    // Nothing needs to be done
+}
+
+int agent(shared_mem_t *mem, int id) {
     printf("Agent %d running!\n", id);
     return 0;
 }
 
 int main(int argc, char **argv) {
+    // Initialize random seed
+    srand(time(NULL));
+
+    // Create shared memory object
+    int fd = shm_open(SHM_NAME,
+            O_CREAT | O_RDWR,
+            S_IRUSR | S_IWUSR);
+    if (fd == -1) {
+        printf("shm_open failed: %s\n", strerror(errno));
+        return -1;
+    }
+
+    // Set shared memory size
+    if (ftruncate(fd, sizeof(shared_mem_t)) == -1) {
+        printf("ftruncate failed: %s\n", strerror(errno));
+        return -1;
+    }
+
+    // Map shared memory to address space
+    shared_mem_t *mem = mmap(NULL,
+            sizeof(shared_mem_t),
+            PROT_READ | PROT_WRITE,
+            MAP_SHARED,
+            fd,
+            0);
+    if (mem == MAP_FAILED) {
+        printf("mmap failed: %s\n", strerror(errno));
+        return -1;
+    }
+
+    initialize_shared_mem(mem);
 
     // Spawn child processes
     for (int i = 0; i < AGENT_COUNT; ++i) {
         pid_t pid = fork();
         if (pid == 0)
-            return agent(i);
+            return agent(mem, i);
     }
 
     // This only runs in parent
@@ -25,6 +76,11 @@ int main(int argc, char **argv) {
     for (int i = 0; i < AGENT_COUNT; ++i)
         wait(NULL);
     printf("All child processes exited.\n");
+
+    // Cleanup and delete shared memory
+    cleanup_shared_mem(mem);
+    munmap(mem, sizeof(shared_mem_t));
+    shm_unlink(SHM_NAME);
 
     return 0;
 }
--- repairmen.h
+++ repairmen.h
@@ -4,5 +4,23 @@
 // Number of repairmen processes
 #define AGENT_COUNT 4
 
+// Width and height of the network grid
+#define GRID_SIZE 7
+
+// Name of the shared memory file in kernel filesystem
+#define SHM_NAME "/repairmen"
+
+// Describes a single cell in the grid
+typedef struct {
+    // True if this cell is fixed, false if it needs to be repaired
+    bool fixed;
+} cell_t;
+
+// Describes the data shared between agents
+typedef struct {
+    // The network cells
+    cell_t grid[GRID_SIZE][GRID_SIZE];
+} shared_mem_t;
+
 #endif // REPAIRMEN_H_
 
